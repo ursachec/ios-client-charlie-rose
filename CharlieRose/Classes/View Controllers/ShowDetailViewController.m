3bd5087c0888a4dcc77615896914e1e64ca6a215
Claudiu-Vlad Ursache <claudiu.ursache@xing.com>
2013-02-16 01:06:39 +0100

    Remove unused method loadDataForShowID.

--- CharlieRose/Classes/View Controllers/ShowDetailViewController.m
+++ CharlieRose/Classes/View Controllers/ShowDetailViewController.m
@@ -166,17 +166,6 @@
 	}
 	
 	self.show = show;
-	[self loadDataForShowID:self.currentShowID];
-}
-
-- (void)loadDataForShowID:(NSString*)showID {
-	
-    [self showLoadingViewAnimated:NO];
-	int64_t delayInSeconds = 1.0;
-	dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, delayInSeconds * NSEC_PER_SEC);
-	dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-		[self hideLoadingViewAnimated:YES];
-	});
 }
 
 - (UIView*)superViewForLoadingView {
