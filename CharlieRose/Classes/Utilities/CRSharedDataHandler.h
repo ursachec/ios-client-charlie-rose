649d3fc3288cdccb97833eec9f6ae7244be9e87d
Claudiu-Vlad Ursache <claudiu.ursache@xing.com>
2013-02-16 01:02:48 +0100

    Add CRSharedDataHandler.

---
+++ CharlieRose/Classes/Utilities/CRSharedDataHandler.h
@@ -0,0 +1,13 @@
+//
+//  CRSharedDataHandler.h
+//  CharlieRose
+//
+//  Created by Claudiu-Vlad Ursache on 16.02.13.
+//  Copyright (c) 2013 Claudiu-Vlad Ursache. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CRSharedDataHandler : NSObject
+
+@end
---
+++ CharlieRose/Classes/Utilities/CRSharedDataHandler.m
@@ -0,0 +1,13 @@
+//
+//  CRSharedDataHandler.m
+//  CharlieRose
+//
+//  Created by Claudiu-Vlad Ursache on 16.02.13.
+//  Copyright (c) 2013 Claudiu-Vlad Ursache. All rights reserved.
+//
+
+#import "CRSharedDataHandler.h"
+
+@implementation CRSharedDataHandler
+
+@end
