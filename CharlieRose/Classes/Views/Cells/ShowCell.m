695434eec44af34d7b3b218621efe229df2522b7
Claudiu-Vlad Ursache <claudiu.ursache@xing.com>
2013-02-18 22:20:31 +0100

    Refactor ShowCell drawing with new font handling.

--- CharlieRose/Classes/Views/Cells/ShowCell.m
+++ CharlieRose/Classes/Views/Cells/ShowCell.m
@@ -46,30 +46,17 @@
 {
     
 #define CELL_PADDING_TOP 5.0f
-    
-#define PADDING_RIGHT 2.0f
-#define PADDING_LEFT 85.0f
-#define PADDING_TOP 10.0f
-#define PADDING_BOTTOM 5.0f
-	
 #define TITLE_WIDTH 160.0f
 #define MAX_TITLE_HEIGHT 60.0f
-#define TITLE_FONT_SIZE 12.0f
-    
-#define DESCRIPTION_WIDTH 190.0f
-#define DESCRIPTION_FONT_SIZE 13.0f
-    
 #define DATE_WIDTH 100.0f
-#define DATE_FONT_SIZE 10.0f
-    
 #define PADDING_FOR_TEXTS 155.0f
-#define PADDING_FOR_DATE_AND_CATEGORY_LABELS 3.0f
     
     //image size: 148 * 96
     CGRect contentRect = self.bounds;
 	
     UIFont *titleFont = [UIFont showCellTitleFont];
     UIFont *dateFont = [UIFont showCellDateFont];
+    UIFont *topicsFont = [UIFont showCellTopicsFont];
     
     CGPoint pointToDraw = CGPointMake(0, 0);
     CGFloat actualFontSize = 0.0f;
@@ -98,15 +85,15 @@
     [[UIColor whiteColor] set];
     CGSize categoryNameSize = CGSizeMake(0, 0);
     NSString *categoryName = _cell.topics; //stringByAppendingFormat:@" ∙ "]
-    size = [categoryName sizeWithFont:dateFont minFontSize:DATE_FONT_SIZE actualFontSize:&actualFontSize forWidth:DATE_WIDTH lineBreakMode:NSLineBreakByTruncatingTail];
+    size = [categoryName sizeWithFont:topicsFont minFontSize:kShowCellTopicsFontSize actualFontSize:&actualFontSize forWidth:DATE_WIDTH lineBreakMode:NSLineBreakByTruncatingTail];
     pointToDraw = CGPointMake(PADDING_FOR_TEXTS, pointToDrawArticleName.y+titleSize2.height+2.0f);
-    size = [categoryName drawAtPoint:pointToDraw forWidth:DATE_WIDTH withFont:dateFont minFontSize:actualFontSize actualFontSize:&actualFontSize lineBreakMode:NSLineBreakByTruncatingTail baselineAdjustment:UIBaselineAdjustmentAlignBaselines];
+    size = [categoryName drawAtPoint:pointToDraw forWidth:DATE_WIDTH withFont:topicsFont minFontSize:actualFontSize actualFontSize:&actualFontSize lineBreakMode:NSLineBreakByTruncatingTail baselineAdjustment:UIBaselineAdjustmentAlignBaselines];
     categoryNameSize = size;
     
     //draw date
     [[UIColor redColor] set];
     NSString *date = _cell.publishingDate;
-    size = [date sizeWithFont:dateFont minFontSize:DATE_FONT_SIZE actualFontSize:&actualFontSize forWidth:DATE_WIDTH lineBreakMode:NSLineBreakByTruncatingTail];
+    size = [date sizeWithFont:dateFont minFontSize:kShowCellDateFontSize actualFontSize:&actualFontSize forWidth:DATE_WIDTH lineBreakMode:NSLineBreakByTruncatingTail];
     pointToDraw = CGPointMake(PADDING_FOR_TEXTS, pointToDrawArticleName.y-size.height);
     size = [date drawAtPoint:pointToDraw forWidth:DATE_WIDTH withFont:dateFont minFontSize:actualFontSize actualFontSize:&actualFontSize lineBreakMode:NSLineBreakByTruncatingTail baselineAdjustment:UIBaselineAdjustmentAlignBaselines];
 }
