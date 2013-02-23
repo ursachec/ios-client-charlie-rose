61055d233302bd09da262112e4e692069d79343f
Claudiu-Vlad Ursache <claudiu.ursache@xing.com>
2013-02-23 19:19:29 +0100

    Add CVUMoviePlayerView.

---
+++ Vendor/CVUMoviePlayerView/CVUMoviePlayerView.h
@@ -0,0 +1,13 @@
+//
+//  CVUMoviePlayerView.h
+//  CharlieRose
+//
+//  Created by Claudiu-Vlad Ursache on 23.02.13.
+//  Copyright (c) 2013 Claudiu-Vlad Ursache. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CVUMoviePlayerView : UIView
+
+@end
---
+++ Vendor/CVUMoviePlayerView/CVUMoviePlayerView.m
@@ -0,0 +1,31 @@
+//
+//  CVUMoviePlayerView.m
+//  CharlieRose
+//
+//  Created by Claudiu-Vlad Ursache on 23.02.13.
+//  Copyright (c) 2013 Claudiu-Vlad Ursache. All rights reserved.
+//
+
+#import "CVUMoviePlayerView.h"
+
+@implementation CVUMoviePlayerView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
